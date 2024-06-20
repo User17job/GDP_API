a031d78f4b8567a8c347e291f442d0827f14a53e
Ramphys Tirana Brito <ramphystiranabrito@gmail.com>
2024-06-20 18:01:54 -0400

    upodate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
     <body class="font-sans antialiased dark:bg-light dark:text-white/50">
         <div class="bg-gray-50 text-light/50 dark:bg-black light:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
+            <img id="background1" class="absolute -left-2000 top-0 max-w-[1000px]" src="https://github.com/User17job/GDP_API/blob/main/Images/Background.png?raw=true" />
 
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
